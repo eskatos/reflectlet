1e491afe6020927d295f57106449aef80e2f1004
Paul Merlin <paul@nosphere.org>
2011-03-31 14:30:11 +0200

    Added forms for POST requests + code cleanup

--- reflectlet/src/main/java/org/codeartisans/reflectlet/Reflectlet.java
+++ reflectlet/src/main/java/org/codeartisans/reflectlet/Reflectlet.java
@@ -11,20 +11,28 @@ import java.util.Enumeration;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Reflectlet extends HttpServlet {
+public class Reflectlet
+        extends HttpServlet
+{
 
-    private static final String HEADER = "<html><head><title>Reflectlet</title></head><body>";
-    private static final String FOOTER = "</body></html>";
+    private static final long serialVersionUID = 1L;
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost( HttpServletRequest req, HttpServletResponse resp )
+            throws ServletException, IOException
+    {
+        doGet( req, resp );
+    }
+
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException
     {
         response.setContentType( "text/html" );
         PrintWriter writer = response.getWriter();
         h1( writer, "Reflectlet" );
 
-        Map<String,Object> data = new LinkedHashMap<String,Object>();
+        Map<String, Object> data = new LinkedHashMap<String, Object>();
 
         h2( writer, "HTTP related data" );
 
@@ -36,10 +44,10 @@ public class Reflectlet extends HttpServlet {
         data.put( "RemoteUser", request.getRemoteUser() );
         data.put( "Protocol", request.getProtocol() );
         data.put( "Method", request.getMethod() );
-        table(writer, data);
+        table( writer, data );
         data.clear();
 
-        h2(writer, "URI & Path related data");
+        h2( writer, "URI & Path related data" );
 
         data.put( "RequestURL", request.getRequestURL() );
         data.put( "RequestURI", request.getRequestURI() );
@@ -48,66 +56,87 @@ public class Reflectlet extends HttpServlet {
         data.put( "PathTranslated", request.getPathTranslated() );
         data.put( "QueryString", request.getQueryString() );
         data.put( "ServletPath", request.getServletPath() );
-        table(writer, data);
+        table( writer, data );
         data.clear();
 
         h2( writer, "HTTP Headers" );
 
+        @SuppressWarnings( "unchecked" )
         Enumeration<String> headerNames = request.getHeaderNames();
         while ( headerNames.hasMoreElements() ) {
             String eachHeaderName = headerNames.nextElement();
+            @SuppressWarnings( "unchecked" )
             Enumeration<String> headerValues = request.getHeaders( eachHeaderName );
             StringBuilder sb = new StringBuilder();
             while ( headerValues.hasMoreElements() ) {
-                sb.append(headerValues.nextElement()).append("\n");
+                sb.append( headerValues.nextElement() ).append( "\n" );
             }
             data.put( eachHeaderName, sb.toString() );
         }
-        table(writer, data);
+        table( writer, data );
         data.clear();
 
-        h2(writer, "Security related data");
+        h2( writer, "Security related data" );
 
         data.put( "Secure", request.isSecure() );
+        data.put( "SSL Cipher", request.getAttribute( "javax.servlet.request.cipher_suite" ) );
+        data.put( "SSL Key Size", request.getAttribute( "javax.servlet.request.key_size" ) );
+        data.put( "SSL SessionID", request.getAttribute( "javax.servlet.request.ssl_session" ) );
         data.put( "ClientCertificate", getClientX509CertificateInfo( request ) );
-        table(writer, data);
+        table( writer, data );
+        data.clear();
+
+        h2( writer, "Request attributes" );
+        @SuppressWarnings( "unchecked" )
+        Enumeration<String> attributeNames = request.getAttributeNames();
+        while ( attributeNames.hasMoreElements() ) {
+            String eachAttributeName = attributeNames.nextElement();
+            data.put( eachAttributeName, request.getAttribute( eachAttributeName ) );
+        }
+        table( writer, data );
         data.clear();
 
         writer.close();
     }
 
-    private void h1( PrintWriter writer, String title ) {
-        wrap(writer, "h1", title);
+    private void h1( PrintWriter writer, String title )
+    {
+        wrap( writer, "h1", title );
     }
 
-    private void h2( PrintWriter writer, String title ) {
+    private void h2( PrintWriter writer, String title )
+    {
         wrap( writer, "h2", title );
     }
 
-    private void table( PrintWriter writer, Map<String,Object> data ) {
+    private void table( PrintWriter writer, Map<String, Object> data )
+    {
         writer.println( "<table border='1'>" );
-        for (Map.Entry<String,Object> eachEntry : data.entrySet()) {
+        for ( Map.Entry<String, Object> eachEntry : data.entrySet() ) {
             writer.println( "<tr><td vertical-align='top'>" + eachEntry.getKey() + "</td><td vertical-align='top'><pre>" + eachEntry.getValue() + "</pre></td></tr>" );
         }
         writer.println( "</table>" );
     }
 
-    private void wrap( PrintWriter writer, String tag, String text ) {
-        writer.println( wrap( tag, text) );
+    private void wrap( PrintWriter writer, String tag, String text )
+    {
+        writer.println( wrap( tag, text ) );
     }
 
-    private String wrap( String tag, String text ) {
+    private String wrap( String tag, String text )
+    {
         return "<" + tag + ">" + text + "</" + tag + ">";
     }
 
-    private String getClientX509CertificateInfo( HttpServletRequest request ) {
+    private String getClientX509CertificateInfo( HttpServletRequest request )
+    {
         Object x509 = request.getAttribute( "javax.servlet.request.X509Certificate" );
         if ( x509 == null ) {
             return "none";
         }
         X509Certificate[] certChain = ( X509Certificate[] ) x509;
-        StringBuffer sb = new StringBuffer();
-        sb.append(certChain.length).append(" certificates in client chain\n");
+        StringBuilder sb = new StringBuilder();
+        sb.append( certChain.length ).append( " certificates in client chain\n" );
         for ( X509Certificate eachCert : certChain ) {
             sb.append( eachCert );
         }
